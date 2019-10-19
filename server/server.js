370cb7337826392245440cd61ca679e694c4d826
Yusof Bandar <ybandar1902@gmail.com>
2019-10-19 15:46:27 +0100

    Init logging server

--- server/server.js
+++ server/server.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.post('/log', (req, res) => {
+    let alpha = req.query.alpha;
+    let beta = req.query.beta;
+    let gamma = req.query.gamma;
     
+    res.sendStatus(200)
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
