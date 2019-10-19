03e62b46d78049b28c633cfa79bac4b10e291bbe
Yusof Bandar <ybandar1902@gmail.com>
2019-10-19 18:58:32 +0100

    Converts training method to async

--- server/neural_network.js
+++ server/neural_network.js
@@ -8,42 +8,50 @@ const fs = require('fs')
 // down [0]
 
 class Model {
-    constructor(){
-        this.design = [3, 15, 15,10,4,8, 4];
+    constructor() {
+        this.design = [3, 15, 15, 10, 4, 8, 4];
         this.brain = new NeuralNetwork(this.design);
-        
-        this.up = [1,0,0,0];
-        this.down = [0,1,0,0];
-        this.right = [0,0,1,0];
-        this.left = [0,0,0,1];
+
+        this.up = [1, 0, 0, 0];
+        this.down = [0, 1, 0, 0];
+        this.right = [0, 0, 1, 0];
+        this.left = [0, 0, 0, 1];
     }
 
-    Train(){
-        this.trainNetworkLogs("./logs/tilt_up_log.json",this.up,this.brain);
-        this.trainNetworkLogs("./logs/tilt_down_log.json",this.down,this.brain);
-        this.trainNetworkLogs("./logs/tilt_right_log.json",this.right,this.brain);
-        this.trainNetworkLogs("./logs/tilt_left_log.json",this.left,this.brain);
+    Train() {
+        let promises = [];
+        promises.push(this.trainNetworkLogs("./logs/tilt_up_log.json", this.up, this.brain));
+        promises.push(this.trainNetworkLogs("./logs/tilt_down_log.json", this.down, this.brain));
+        promises.push(this.trainNetworkLogs("./logs/tilt_right_log.json", this.right, this.brain));
+        promises.push(this.trainNetworkLogs("./logs/tilt_left_log.json", this.left, this.brain));
+
+        return Promise.all(promises)
     }
 
-    Predit(data){
+    Predit(data) {
         return this.brain.predict(data);
     }
 
-    
 
-    trainNetworkLogs(path,result,brain){
-        this.importLogs(path).then((data)=>{
-            data.forEach(log => {
-                brain.train(log,result);
-            });
-        }).catch((err) =>{
-            console.log(err);
+
+    trainNetworkLogs(path, result, brain) {
+        return new Promise((resolve, reject) => {
+            this.importLogs(path).then((data) => {
+                data.forEach(log => {
+                    brain.train(log, result);
+                    resolve();
+                });
+            }).catch((err) => {
+                console.log(err);
+                reject();
+            })
         })
+
     }
 
-    importLogs(path){
+    importLogs(path) {
         return new Promise((resolve, reject) => {
-            fs.readFile(path,(err,data)=>{
+            fs.readFile(path, (err, data) => {
                 let logs = [];
                 if (err) {
                     reject(err);
@@ -57,12 +65,15 @@ class Model {
             })
         })
     }
-} 
+}
 
 
 
 let model = new Model();
-model.Train();
+model.Train().then((data) => {
+    console.log("done");
+});
+
 
 
 
