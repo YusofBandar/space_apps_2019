844513812cad2b018a49b3bfffda16acd401f526
Yusof Bandar <ybandar1902@gmail.com>
2019-10-19 18:47:27 +0100

    Converted neural model to class

--- server/neural_network.js
+++ server/neural_network.js
@@ -3,38 +3,41 @@
 const NeuralNetwork = require('./lib/snn');
 const fs = require('fs')
 
-let design = [3, 4, 3, 4];
-let brain = new NeuralNetwork(design);
 
-
-TrainNetworkLogs(brain);
-
-//largest = brain.predict([10, 20]);
-//console.log(`Probability Score for Largest: ${largest}`)
-
-function TrainNetworkLogs(brain) {
-    importLogs("./tilt_left_log.json").then((data)=>{
-        data.forEach(log => {
-            brain.train(log,[1,0,0,0]);
-        });
-        console.log(brain.predict([60,55,21]));
-    }).catch((err) =>{
-        console.log(err);
-    })
-}
-function importLogs(path) {
-    return new Promise((resolve, reject) => {
-        fs.readFile(path,(err,data)=>{
-            let logs = [];
-            if (err) {
-                reject(err);
-            } else {
-                let json = JSON.parse(data);
-                json.forEach(element => {
-                    logs.push([element.alpha, element.beta, element.gamma]);
-                });
-                return resolve(logs);
-            }
+module.exports = class Model {
+    constructor(){
+        this.design = [3, 15, 15,10,4,8, 4];
+        this.brain = new NeuralNetwork(design);
+    }
+    
+    trainNetworkLogs(path,result,brain){
+        this.importLogs(path).then((data)=>{
+            data.forEach(log => {
+                brain.train(log,result);
+            });
+        }).catch((err) =>{
+            console.log(err);
         })
-    })
-}
+    }
+
+    importLogs(path){
+        return new Promise((resolve, reject) => {
+            fs.readFile(path,(err,data)=>{
+                let logs = [];
+                if (err) {
+                    reject(err);
+                } else {
+                    let json = JSON.parse(data);
+                    json.forEach(element => {
+                        logs.push([element.alpha, element.beta, element.gamma]);
+                    });
+                    return resolve(logs);
+                }
+            })
+        })
+    }
+} 
+
+
+
+
